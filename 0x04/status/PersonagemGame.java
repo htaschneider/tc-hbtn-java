7de32283158712df7961daced263d498fb2b369e
Pedro Schneider <wschneider@ciandt.com>
2022-06-13 14:51:36 -0300

    add PersonagemGame and Program classes

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,46 @@
+public class PersonagemGame {
+
+  private int saudeAtual;
+  private String nome;
+  private String status;
+
+  public int getSaudeAtual() {
+    return saudeAtual;
+  }
+
+  public void setSaudeAtual(int saudeAtual) {
+    this.saudeAtual = saudeAtual;
+    if (saudeAtual == 0) {
+      status = "morto";
+    } else {
+      status = "vivo";
+    }
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public void tomarDano(int quantidadeDeDano) {
+    this.saudeAtual = Math.max(0, saudeAtual - quantidadeDeDano);
+    if (saudeAtual == 0) {
+      status = "morto";
+    }
+  }
+
+  public void receberCura(int quantidadeDeCura) {
+    this.saudeAtual = Math.min(100, saudeAtual + quantidadeDeCura);
+    if (saudeAtual > 0 && !status.equals("vivo")) {
+      status = "vivo";
+    }
+  }
+  
+  public String getStatus() {
+    return status;
+  }
+
+}
