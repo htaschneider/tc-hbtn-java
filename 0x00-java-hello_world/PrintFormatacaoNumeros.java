dfc01fd18c19301a01b1c4f6c392095d359ad13f
Pedro Schneider <wschneider@ciandt.com>
2022-06-13 12:04:27 -0300

    add PrintFormatacaoNumeros class

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,8 +1,11 @@
+import java.util.Locale;
+import java.text.NumberFormat;
+
 public class PrintFormatacaoNumeros {
   public static void main(String[] args) {
     float taxa = 0.2456f;
     float valor = 7654.321f;
-    System.out.printf("Valor: R$ %.2f%%\n", valor);
+    System.out.println(NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(valor));
     System.out.printf("Taxa: %.2f%%\n", taxa);
   }
 }
