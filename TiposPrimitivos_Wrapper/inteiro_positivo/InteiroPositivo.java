046c2307a4c271b5639ec7823f558d15aa6915a2
Pedro Schneider <wschneider@ciandt.com>
2022-06-13 14:04:28 -0300

    add InteiroPositivo and Program classes

---
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,48 @@
+public class InteiroPositivo {
+
+  private int valor;
+
+  public InteiroPositivo(int valor) {
+    if (valor < 0) {
+      throw new RuntimeException("Valor nao eh um valor inteiro positivo");
+    }
+    this.valor = valor;
+  }
+
+  public InteiroPositivo(String valor) {
+    try {
+      int val = Integer.parseInt(valor);
+      if (val < 0) {
+        throw new RuntimeException("Valor nao eh um valor inteiro positivo");
+      }
+      this.valor = val;
+
+    } catch (NumberFormatException ex) {
+      throw new RuntimeException("Valor nao eh um valor inteiro positivo");
+    }
+  }
+
+  public int getValor() {
+    return this.valor;
+  }
+
+  public void setValor(int novo_valor) throws RuntimeException {
+    if (novo_valor < 0) {
+      throw new RuntimeException("Valor nao eh um valor inteiro positivo");
+    }
+    this.valor = novo_valor;
+  }
+
+  public boolean ehPrimo() {
+    if (valor <= 1) {
+      return false;
+    }
+    for (int i = 2; i < valor; i++) {
+      if (valor % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
