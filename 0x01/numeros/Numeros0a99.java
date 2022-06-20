ad15d83bd91e8081055a2f0d3887bd1655d7fa7d
Pedro Schneider <wschneider@ciandt.com>
2022-06-20 10:02:22 -0300

    add Numbers class

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,11 @@
+
+public class Numeros0a99 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 99; i++) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("99");
+    }
+}
