43238f25c6cee843592b5502f2d62b8367792c96
Pedro Schneider <wschneider@ciandt.com>
2022-06-22 11:42:35 -0300

    add rectangle class

--- 0x05/retangulo_completo/Retangulo.java
+++ 0x05/retangulo_completo/Retangulo.java
@@ -32,6 +32,6 @@ public class Retangulo extends FormaGeometrica {
 
     @Override
     public String toString() {
-        return "[Retangulo] " + this.largura + " / " + this.altura;
+        return String.format("[Retangulo] %.2f / %.2f", this.largura, this.altura);
     }
 }
