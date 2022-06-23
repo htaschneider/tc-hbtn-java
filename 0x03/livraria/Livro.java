50ee6e17b1dd49daadd3d08b35ce7fb2d8369209
Pedro Schneider <wschneider@ciandt.com>
2022-06-23 14:39:00 -0300

    add library package

---
+++ 0x03/livraria/Livro.java
@@ -0,0 +1,49 @@
+import exceptions.*;
+
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private double preco;
+
+    public Livro(String titulo, String autor, double preco) throws Exception {
+        setTitulo(titulo);
+        setAutor(autor);
+        setPreco(preco);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setTitulo(String titulo) throws LivroInvalidoException {
+        if (titulo.length() < 3) {
+            throw new LivroInvalidoException("Titulo de livro invalido");
+        }
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) throws AutorInvalidoException {
+        if (autor.split(" ").length < 2) {
+            throw new AutorInvalidoException("Nome de autor invalido");
+        }
+        this.autor = autor;
+    }
+
+    public void setPreco(double preco) throws LivroInvalidoException {
+        if (!(preco > 0.0)) {
+            throw new LivroInvalidoException("Preco de livro invalido");
+        }
+        this.preco = preco;
+    }
+
+
+}
