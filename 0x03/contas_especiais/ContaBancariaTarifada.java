4af8ec63198c909086a4c524d49ab4f7c258d697
Pedro Schneider <wschneider@ciandt.com>
2022-06-27 13:20:02 -0300

    add special account class

--- 0x03/contas_especiais/ContaBancariaTarifada.java
+++ 0x03/contas_especiais/ContaBancariaTarifada.java
@@ -1,28 +1,28 @@
 import exceptions.*;
 
 public class ContaBancariaTarifada extends ContaBancariaBasica {
-    private int quantidadeDeTransacoes;
+    private int quantidadeTransacoes;
 
     public ContaBancariaTarifada(String numeracao, double taxaJurosAnual) {
         super(numeracao, taxaJurosAnual);
-        this.quantidadeDeTransacoes = 0;
+        this.quantidadeTransacoes = 0;
     }
 
     public int getQuantidadeDeTransacoes() {
-        return quantidadeDeTransacoes;
+        return quantidadeTransacoes;
     }
 
     @Override
     public void sacar(double valor) throws OperacaoInvalidaException {
         super.sacar(valor);
         super.sacar(0.10);
-        quantidadeDeTransacoes++;
+        quantidadeTransacoes++;
     }
 
     @Override
     public void depositar(double valor) throws OperacaoInvalidaException {
         super.depositar(valor);
         super.sacar(0.10);
-        quantidadeDeTransacoes++;
+        quantidadeTransacoes++;
     }
 }
