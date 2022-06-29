f7b4934928f888de799b324cf78a2c9e246cba82
Pedro Schneider <wschneider@ciandt.com>
2022-06-28 22:50:26 -0300

    add order resume

---
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+import produtos.Dvd;
+import produtos.Livro;
+import produtos.Produto;
+
+public class Pedido {
+
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(int percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+    public double totalSemDesconto() {
+        return Arrays.asList(itens).stream()
+                .mapToDouble(item -> item.getQuantidade() * item.getProduto().obterPrecoLiquido())
+                .sum();
+    }
+
+    public double calcularTotal() {
+        return totalSemDesconto() * (1 - percentualDesconto / 100);
+    }
+
+    public void apresentarResumoPedido() {
+        System.out.println("------- RESUMO PEDIDO -------");
+
+        for (ItemPedido item : itens) {
+            Produto produto = item.getProduto();
+            if (produto instanceof Livro) {
+                Livro livro = (Livro) produto;
+                System.out.printf("Tipo: Livro  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f\n",
+                        livro.getTitulo(), livro.getPreco(), item.getQuantidade(), livro.getPreco());
+            } else {
+                Dvd dvd = (Dvd) produto;
+                System.out.printf("Tipo: Dvd  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f\n",
+                        dvd.getTitulo(), dvd.getPreco(), item.getQuantidade(), dvd.getPreco());
+
+            }
+        }
+        System.out.println("\n----------------------------");
+        System.out.printf("DESCONTO: %.2f\n", totalSemDesconto() - calcularTotal());
+        System.out.printf("TOTAL PPRODUTOS: %.2f\n", totalSemDesconto());
+        System.out.println("----------------------------");
+        System.out.printf("TOTAL PEDIDO: %.2f\n", calcularTotal());
+        System.out.println("----------------------------");
+    }
+
+}
---
+++ Heranca_Exercicios/resumo_pedido/produtos/Livro.java
@@ -0,0 +1,45 @@
+package produtos;
+
+public class Livro extends Produto {
+
+    private int paginas;
+    private String autor;
+    private int edicao;
+
+    public Livro(String titulo, int ano, String pais, double precoBruto, int paginas, String autor, int edicao) {
+        super(titulo, ano, pais, precoBruto);
+        this.paginas = paginas;
+        this.autor = autor;
+        this.edicao = edicao;
+    }
+
+    @Override
+    public double obterPrecoLiquido() {
+        return super.precoBruto * 1.15;
+    }
+
+    public String getTitulo() {
+        return super.titulo;
+    }
+
+    public String getPais() {
+        return super.pais;
+    }
+
+    public double getPreco() {
+        return super.precoBruto;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+}
