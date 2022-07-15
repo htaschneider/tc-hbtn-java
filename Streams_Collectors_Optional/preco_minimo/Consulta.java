3241265fbdaf655ac6f5d741166dd8c24debce79
Pedro Schneider <wschneider@ciandt.com>
2022-07-15 09:39:24 -0300

    add obterProdutosPorPrecoMinimo

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(p -> p.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream()
+                .max((p1, p2) -> Double.compare(p1.getPreco(), p2.getPreco()))
+                .get();
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        return produtos.stream()
+                .filter(p -> p.getPreco() >= precoMinimo)
+                .collect(Collectors.toList());
+    }
+}
